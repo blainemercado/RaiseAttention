a68000bf9c19e9882c53a5428dc885f7881c9994
Blaine Mercado <blainemercado@hotmail.com>
2017-01-04 16:31:03 -0800

    added admin login

--- apps/adminDashboard/urls.py
+++ apps/adminDashboard/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^admin$', views.admin, name = 'adminDash')
+	url(r'^admin$', views.admin, name = 'admin'),
+	url(r'^login$', views.login, name = 'adminLogin'),
+	url(r'^logout$', views.logout, name = 'adminLogout'),
+	url(r'^adminDash$', views.adminDash, name = 'adminDash')
 ]
--- apps/adminDashboard/views.py
+++ apps/adminDashboard/views.py
@@ -5,16 +5,36 @@ from django.core.urlresolvers import reverse
 
 # Create your views here.
 def admin(request):
-	npoArray = Npo.npoManager.voteCount()
-	supportersArray = Supporter.supporterManager.getSupporters()
-	visitorsArray = SiteVisitors.visitorsManager.getVisitors()
-	for npo in npoArray:
-		print("total votes for ", npo.name, npo.votes)
-	for supporter in supportersArray:
-		print(supporter.ageRange)
-	context = {
-		"npos": npoArray,
-		"supporters": supportersArray,
-		"visitors": visitorsArray
-	}
-	return render(request, 'adminDashboard/admin.html', context)
+	return render(request, 'adminDashboard/adminLoginPage.html')
+
+def login(request):
+	if request.method != 'POST':
+		return redirect('/adminDashboard/admin')
+	password = request.POST['password']
+	if password == "0Dec0Hat":
+		request.session['password'] = password
+		return redirect('/adminDashboard/adminDash')
+	else:
+		return redirect('/adminDashboard/admin')
+
+def logout(request):
+	del request.session['password']
+	return redirect('/adminDashboard/admin')
+
+def adminDash(request):
+	if 'password' not in request.session:
+		return redirect('/adminDashboard/admin')
+	else:
+		npoArray = Npo.npoManager.voteCount()
+		supportersArray = Supporter.supporterManager.getSupporters()
+		visitorsArray = SiteVisitors.visitorsManager.getVisitors()
+		for npo in npoArray:
+			print("total votes for ", npo.name, npo.votes)
+		for supporter in supportersArray:
+			print(supporter.ageRange)
+		context = {
+			"npos": npoArray,
+			"supporters": supportersArray,
+			"visitors": visitorsArray
+		}
+		return render(request, 'adminDashboard/admin.html', context)
